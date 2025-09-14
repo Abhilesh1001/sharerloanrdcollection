b8f37e601bcbd1a205ae5d719ab225fb86047399
Abhilesh1001 <abhilesh925@gmail.com>
2025-09-14 21:04:12 +0530

    up1

--- .scripts/deploy.sh
+++ .scripts/deploy.sh
@@ -3,9 +3,16 @@ set -e
 
 echo "Deployment started..."
 
+# Fix Git safe directory issue
+git config --global --add safe.directory /var/www/sharerloanrdcollection
+
+# Go to project folder
+cd /var/www/sharerloanrdcollection
+
 # Pull the latest version of the app
-git pull origin main
-echo "New changes copied to server !"
+git fetch origin main
+git reset --hard origin/main
+echo "New changes copied to server!"
 
 echo "Installing Dependencies..."
 npm install --yes
